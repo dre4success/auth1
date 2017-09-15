0fdb58e020c2e3081eeb4041d0deddf3b8b867f0
Damilare Joseph <dre4success@live.com>
2017-09-15 15:00:09 +0100

    mounting routes

--- index.js
+++ index.js
@@ -3,12 +3,13 @@ const http = require('http');
 const bodyParser = require('body-parser');
 const morgan = require('morgan');
 const app = express();
+const router = require('./routes/router');
 
 
 // App Setup
 app.use(morgan('combined')); // logging incoming request
 app.use(bodyParser.json({type: '*/*'})) // parse incoming request no matter what the type is
-
+router(app);
 
 //Server Setup
 const port = process.env.PORT || 3090;
