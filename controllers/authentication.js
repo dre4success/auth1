19118e3a2a6b0961ab83e6aff01a74f2312880a3
Damilare Joseph <dre4success@live.com>
2017-09-15 16:15:32 +0100

    authentication controller

--- controllers/authentication.js
+++ controllers/authentication.js
@@ -1,3 +1,25 @@
-exports.signup = (req, res, next) => {
-	res.send({success: 'true'});
-}
+const User = require('../models/user');
+
+exports.signup = async (req, res, next) => {
+	try {
+		const email = req.body.email;
+		const password = req.body.password;
+
+		// See if a user with the given email exists
+		const existingUser = await User.findOne({ email });
+
+		// If a user with email does exist, return an error
+		if (existingUser) {
+			return res.status(422).send({ error: 'Email already exists' });
+		}
+
+		// If a user with email does Not exist, create and save user record
+		const user = await new User({ email, password }).save();
+
+		// Respond to request indicating the user was created
+		res.json({ success: true });
+		
+	} catch (err) {
+		res.status(400).send(err);
+	}
+};
