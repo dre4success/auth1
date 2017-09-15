a94087422456e8398c4df0ac53df4afa5bab1bde
Damilare Joseph <dre4success@live.com>
2017-09-15 16:25:15 +0100

    encrypting password

--- models/User.js
+++ models/User.js
@@ -1,12 +1,33 @@
 const mongoose = require('mongoose');
 mongoose.Promise = global.Promise;
 const Schema = mongoose.Schema;
+const bcrypt = require('bcrypt-nodejs');
 
 const userSchema = new Schema({
 	email: { type: String, unique: true, lowercase: true },
 	password: String
-})
+});
 
-const modelClass = mongoose.model('user', userSchema)
+// On Save Hook, encrypt password
+userSchema.pre('save', function(next) {
+	const user = this;
+
+	bcrypt.genSalt(10, function(err, salt) {
+		if (err) {
+			return next(err);
+		}
+
+		bcrypt.hash(user.password, salt, null, function(err, hash) {
+			if (err) {
+				return next(err);
+			}
+
+			user.password = hash;
+			next();
+		});
+	});
+});
+
+const modelClass = mongoose.model('user', userSchema);
 
 module.exports = modelClass;
