87a4ff50248d2d5f204b15b9fbf998a4405ad158
Damilare Joseph <dre4success@live.com>
2017-09-15 22:01:55 +0100

    bcrypt compare

--- models/User.js
+++ models/User.js
@@ -1,7 +1,9 @@
 const mongoose = require('mongoose');
 mongoose.Promise = global.Promise;
 const Schema = mongoose.Schema;
-const bcrypt = require('bcrypt-nodejs');
+const Promise = require('bluebird');
+
+const bcrypt = Promise.promisifyAll(require('bcrypt-nodejs'));
 
 const userSchema = new Schema({
 	email: { type: String, unique: true, lowercase: true },
@@ -9,14 +11,19 @@ const userSchema = new Schema({
 });
 
 // On Save Hook, encrypt password
+// Before saving this model, run this function
+
 userSchema.pre('save', function(next) {
+	// get access to the user model
 	const user = this;
 
+	// generate a salt then run callback
 	bcrypt.genSalt(10, function(err, salt) {
 		if (err) {
 			return next(err);
 		}
 
+		// hash (encrypt) password using the salt
 		bcrypt.hash(user.password, salt, null, function(err, hash) {
 			if (err) {
 				return next(err);
@@ -27,6 +34,20 @@ userSchema.pre('save', function(next) {
 		});
 	});
 });
+
+/*userSchema.methods.comparePassword = function(canditatePassword, callback) {
+	bcrypt.compare(candidatePassword, this.password, function(err, isMatch) {
+		if(err) { return callback(err); }
+
+		callback(null, isMatch)
+	})
+}*/
+
+userSchema.methods.comparePassword = function (candidatePassword) {
+
+	return bcrypt.compareAsync(candidatePassword, this.password);		
+
+}
 
 const modelClass = mongoose.model('user', userSchema);
 
