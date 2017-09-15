d7169e87ee94e4a4403af61684184c345efa0a9e
Damilare Joseph <dre4success@live.com>
2017-09-15 15:04:22 +0100

    exporting user model

--- models/User.js
+++ models/User.js
@@ -3,6 +3,10 @@ mongoose.Promise = global.Promise;
 const Schema = mongoose.Schema;
 
 const userSchema = new Schema({
-	email: String,
+	email: { type: String, unique: true, lowercase: true },
 	password: String
 })
+
+const modelClass = mongoose.model('user', userSchema)
+
+module.exports = modelClass;
